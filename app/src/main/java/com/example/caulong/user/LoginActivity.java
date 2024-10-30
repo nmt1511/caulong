3ff5186918865bbc9f2891b34feecec36e28e61e
nmt1511 <nmthuan175@gmail.com>
2024-10-30 16:24:56 +0700

    them code signup,share,quenmk
    cap nhat code

--- app/src/main/java/com/example/caulong/user/LoginActivity.java
+++ app/src/main/java/com/example/caulong/user/LoginActivity.java
@@ -83,6 +83,7 @@ public class LoginActivity extends AppCompatActivity {
                         Toast.makeText(getApplication(), "Mật khẩu hợp lệ", Toast.LENGTH_LONG).show();
                         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                         startActivity(intent);
+                        finish();
                     }else {
                         Toast.makeText(getApplication(), "Mật khẩu không hợp lệ", Toast.LENGTH_LONG).show();
                         edtUser.requestFocus();
