6ea50ece1c16f3e269414997e4cb2e6fc00aa51f
brianmhoang <165451083+brianmhoang@users.noreply.github.com>
2024-12-06 18:19:48 +0700

    Hoang update Database for THuannnnnnnnnn

--- app/src/main/java/com/example/caulong/data/DataDatSan.java
+++ app/src/main/java/com/example/caulong/data/DataDatSan.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class DataDatSan extends SQLiteOpenHelper {
     // Tên cơ sở dữ liệu và phiên bản
     private static final String DATABASE_NAME = "booking_management.db";
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 1;
 
     public DataDatSan(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -35,6 +35,13 @@ public class DataDatSan extends SQLiteOpenHelper {
                 "court_name TEXT NOT NULL, " +
                 "price REAL, " +
                 "status TEXT)");
+        sql = "INSERT INTO Court (court_name, price, status) " +
+                "VALUES ('Sân 1', 25000.0, 'available')," +
+                "('Sân 2', 25000.0, 'available'),"+
+                "('Sân 3', 25000.0, 'available')," +
+                "('Sân 4', 25000.0, 'available'),"+
+                "('Sân 5', 25000.0, 'available')";
+        db.execSQL(sql);
 
         // Tạo bảng User
         db.execSQL("CREATE TABLE IF NOT EXISTS User (" +
@@ -42,6 +49,10 @@ public class DataDatSan extends SQLiteOpenHelper {
                 "username TEXT NOT NULL UNIQUE, " +
                 "password TEXT NOT NULL, " +
                 "role INTEGER NOT NULL)");
+        sql = "INSERT INTO User (username, password, role) " +
+                "VALUES ('hoanghm','123456', 1)," +
+                "('thuannm','123456', 0)" ;
+        db.execSQL(sql);
 
         // Tạo bảng Customer
         db.execSQL("CREATE TABLE IF NOT EXISTS Customer (" +
@@ -50,9 +61,12 @@ public class DataDatSan extends SQLiteOpenHelper {
                 "gender REAL, " +
                 "phone_number TEXT, " +
                 "email TEXT, " +
+                "avatar TEXT," +
                 "user_id INTEGER, " +
                 "FOREIGN KEY(user_id) REFERENCES User(user_id))");
-
+        sql = "INSERT INTO Customer(customer_name,phone_number,email,user_id)"+
+                "VALUES ('Nguyễn Minh Thuận','0347023834','thuannm@gmail.com',2)";
+        db.execSQL(sql);
 
         // Tạo bảng Admin
         db.execSQL("CREATE TABLE IF NOT EXISTS Admin (" +
@@ -61,13 +75,33 @@ public class DataDatSan extends SQLiteOpenHelper {
                 "email TEXT, " +
                 "user_id INTEGER, " +
                 "FOREIGN KEY(user_id) REFERENCES User(user_id))");
+        sql = "INSERT INTO Admin(admin_name,email,user_id)"+
+                "VALUES ('Hoàng','hoanghm4869@gmail.com',1)";
+        db.execSQL(sql);
 
         // Tạo bảng Time
         db.execSQL("CREATE TABLE IF NOT EXISTS Time (" +
                 "time_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 "time_name TEXT, " +
                 "value REAL)");
-
+        sql = "INSERT INTO Time(time_name, value)"+
+                "VALUES ('7:00',0.5),('7:30',0.5)," +
+                "('8:00',0.5),('8:30',0.5)," +
+                "('9:00',0.5),('9:30',0.5)," +
+                "('10:00',0.5),('10:30',0.5)," +
+                "('11:00',0.5),('11:30',0.5)," +
+                "('12:00',0.5),('12:30',0.5)," +
+                "('13:00',0.5),('13:30',0.5)," +
+                "('14:00',0.5),('14:30',0.5)," +
+                "('15:00',0.5),('15:30',0.5)," +
+                "('16:00',0.5),('16:30',0.5)," +
+                "('17:00',0.5),('17:30',0.5)," +
+                "('18:00',0.5),('18:30',0.5)," +
+                "('19:00',0.5),('19:30',0.5)," +
+                "('20:00',0.5),('20:30',0.5)," +
+                "('21:00',0.5),('21:30',0.5)," +
+                "('22:00',0.5),('22:30',0.5)";
+        db.execSQL(sql);
 
         // Tạo bảng Booking
         db.execSQL("CREATE TABLE IF NOT EXISTS Booking (" +
@@ -86,7 +120,9 @@ public class DataDatSan extends SQLiteOpenHelper {
         db.execSQL("CREATE TABLE IF NOT EXISTS Service_type (" +
                 "type_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 "type_name TEXT)");
-
+        sql = "INSERT INTO Service_type(type_name)"+
+                "VALUES ('Thức ăn'),('Nước uống'),('Dịch vụ khác')";
+        db.execSQL(sql);
 
         // Tạo bảng Service
         db.execSQL("CREATE TABLE IF NOT EXISTS Service (" +
@@ -96,7 +132,11 @@ public class DataDatSan extends SQLiteOpenHelper {
                 "type_id INTEGER NOT NULL, " +
                 "service_price REAL,"+
                 "FOREIGN KEY(type_id) REFERENCES Service_type(type_id))");
-
+        sql = "INSERT INTO Service(service_name,quantity,type_id, service_price)"+
+                "VALUES ('Bánh mì',50,1,8000),('Mì ly Modern',20,1,12000)," +
+                "('Nước suối',150,2,5000),('Revive',150,2,10000)," +
+                "('Revive chanh muối',150,2,10000),('Cầu',300,3,20000)" ;
+        db.execSQL(sql);
 
         // Tạo bảng Booking_time
         db.execSQL("CREATE TABLE IF NOT EXISTS Booking_time (" +
@@ -150,20 +190,16 @@ public class DataDatSan extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // Xóa bảng cũ nếu cơ sở dữ liệu được nâng cấp
-//        db.execSQL("DROP TABLE IF EXISTS Court");
-//        db.execSQL("DROP TABLE IF EXISTS User");
-//        db.execSQL("DROP TABLE IF EXISTS Customer");
-//        db.execSQL("DROP TABLE IF EXISTS Admin");
-//        db.execSQL("DROP TABLE IF EXISTS Time");
-//        db.execSQL("DROP TABLE IF EXISTS Booking_time");
-//        db.execSQL("DROP TABLE IF EXISTS Booking");
-//        db.execSQL("DROP TABLE IF EXISTS Booking_service");
-//        db.execSQL("DROP TABLE IF EXISTS Service");
-//        db.execSQL("DROP TABLE IF EXISTS Payment");
-        if (oldVersion < 2) { // Kiểm tra nếu phiên bản cũ hơn 2
-            // Thêm cột avatar vào bảng Customer
-            db.execSQL("ALTER TABLE Customer ADD COLUMN avatar TEXT");
-        }
+        db.execSQL("DROP TABLE IF EXISTS Court");
+        db.execSQL("DROP TABLE IF EXISTS User");
+        db.execSQL("DROP TABLE IF EXISTS Customer");
+        db.execSQL("DROP TABLE IF EXISTS Admin");
+        db.execSQL("DROP TABLE IF EXISTS Time");
+        db.execSQL("DROP TABLE IF EXISTS Booking_time");
+        db.execSQL("DROP TABLE IF EXISTS Booking");
+        db.execSQL("DROP TABLE IF EXISTS Booking_service");
+        db.execSQL("DROP TABLE IF EXISTS Service");
+        db.execSQL("DROP TABLE IF EXISTS Payment");
         onCreate(db);
     }
 
